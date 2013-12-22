e1b1049545892f28832b519e557076311c623c15
matthewwoo <matthew.edan.woo@gmail.com>
2013-12-22 14:47:42 -0500

    Added AWS S3 Services & Paperclip to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,16 @@ Omrails::Application.configure do
   # Required for heroku
   # Note to set actual host
   config.action_mailer.default_url_options = { :host => 'boldesttea.com' }
+ 
+  # Sets Paperclip to upload images to Amazon S3 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+  
+  
 end
